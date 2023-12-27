d97472f9d38735d7061e1099dac8d3d805c44d3b
huwgoddard <111214210+huwgoddard@users.noreply.github.com>
2023-12-28 10:08:27 +1100

    squares populate

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('yep')
+
+
+
+let sideLength=16;
+
+let mainContainer=document.querySelector('#main-container');
+
+for (let i=0;i<sideLength**2;i++) {
+    let square=document.createElement('div');
+    square.style.backgroundColor= 'pink';
+    square.style.height='60px';
+    square.style.width='60px';
+    mainContainer.appendChild(square);
+    console.log(i);
+}

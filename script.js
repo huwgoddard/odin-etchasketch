c711d89f571821a03094b98d6e0f7939a6267e58
huwgoddard <111214210+huwgoddard@users.noreply.github.com>
2023-12-28 12:51:30 +1100

    added button to html, moved canvas to function

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
-
-
-let sideLength=16;
-
 let mainContainer=document.querySelector('#main-container');
 
 function mouseover(event) {
@@ -11,11 +6,16 @@ function mouseover(event) {
     }
 }
 
-for (let i=0;i<sideLength**2;i++) {
+function setSize(canvasSideLength=16) {
+    squareSideLength = 960/canvasSideLength;
+    for (let i=0;i<canvasSideLength**2;i++) {
     let square=document.createElement('div');
-    square.style.backgroundColor= 'pink';
-    square.style.height='60px';
-    square.style.width='60px';
-    square.addEventListener('mouseover',mouseover)
-    mainContainer.appendChild(square);
+        square.style.backgroundColor= 'pink';
+        square.style.height=squareSideLength.toString()+'px';
+        square.style.width=square.style.height;
+        square.addEventListener('mouseover',mouseover)
+        mainContainer.appendChild(square);
+    }
 }
+
+setSize(16);

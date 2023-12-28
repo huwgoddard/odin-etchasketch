a3b7047aba5e12eb2eb2d485e8d582462af2e154
huwgoddard <111214210+huwgoddard@users.noreply.github.com>
2023-12-28 11:06:16 +1100

    squares change colour on mouseon

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ let sideLength=16;
 
 let mainContainer=document.querySelector('#main-container');
 
+function mouseover(event) {
+    let target = event.target;
+    console.log(target);
+    target.style.backgroundColor='black';
+}
+
 for (let i=0;i<sideLength**2;i++) {
     let square=document.createElement('div');
     square.style.backgroundColor= 'pink';
     square.style.height='60px';
     square.style.width='60px';
+    square.addEventListener('mouseover',mouseover)
     mainContainer.appendChild(square);
     console.log(i);
 }
